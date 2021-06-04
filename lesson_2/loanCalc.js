fad8e3c9f2ca74189afd4f2bc323421a992509af
Edris <edris.atamy@gmail.com>
2021-06-04 05:02:13 +0000

    changes made to loanCalc.JS recommended by Code Review

--- lesson_2/loanCalc.js
+++ lesson_2/loanCalc.js
@@ -1,17 +1,24 @@
 const READLINE = require('readline-sync');
-let repeat = '';
+let answer = '';
 let loanMonths = years => years * 12;
 
 let greet = {
-  welcome: 'Welcome to loanCalc.JS!',
-  exit: 'Thanks for using loanCalc.JS!'
+  welcome: '---------- Welcome to loanCalc.JS! ----------',
+  exit: '---------- Thanks for using loanCalc.JS! ----------'
 };
 
 function findRate(loan, duration, interest) {
   let loanAmount = loan;
   let loanDuration = duration;
-  let percentRate = +interest;
-  let monthlyPayment = loanAmount * (percentRate / (1 - Math.pow((1 + percentRate), (-loanDuration))));
+  let percentRate = interest;
+  let monthlyPayment;
+
+  if (percentRate === 0) {
+    monthlyPayment = loanAmount / loanDuration;
+  } else {
+    monthlyPayment = loanAmount *
+    (percentRate / (1 - Math.pow((1 + percentRate), (-loanDuration))));
+  }
   return 'Your monthly payment is $' + monthlyPayment.toFixed(2);
 }
 
@@ -30,10 +37,7 @@ function invalid(input) {
   return Number.isNaN(Number(input)) || input <= 0;
 }
 
-console.log(greet.welcome);
-
-do {
-
+function getLoan() {
   console.log('What is the Loan amount?');
   let loan = READLINE.prompt();
 
@@ -41,34 +45,57 @@ do {
     console.log('Invalid input, please input a positive integer amount:');
     loan = READLINE.prompt();
   }
+  return Number(loan);
+}
 
+function getLoanDuration() {
   console.log('What is the loan duration? (In Years)');
   let loanTime = READLINE.prompt();
 
-  while (invalid(loanTime)) {
-    console.log('Invalid input, please input a positive integer amount (In Years)');
+  while (loanTime % 1 !== 0) {
+    console.log('Invalid input, please input a positive, whole integer amount (In Years)');
     loanTime = READLINE.prompt();
   }
   loanTime = loanMonths(loanTime);
+  return loanTime;
+}
 
+function getAPR() {
   console.log('What is the APR of the loan? (Can input as whole number or decimal)');
   let monthlyInterest = READLINE.prompt();
 
-  while (invalid(monthlyInterest)) {
-    console.log('Invalid input, please input a positive integer amount for the APR: (Can input as whole number or decimal)');
+  while (monthlyInterest < 0) {
+    console.log('Invalid input, please input a positive integer amount for the APR: (Can input as whole number, decimal, or 0 for no interest loans)');
     monthlyInterest = READLINE.prompt();
   }
   monthlyInterest = interestRate(monthlyInterest);
+  return monthlyInterest;
+}
 
-  console.log(findRate(loan, loanTime, monthlyInterest));
-
+function repeat() {
   console.log('Do you want to calculate another loan? (\'yes\' or \'no\')');
-  repeat = READLINE.prompt().toLowerCase();
+  let answer = READLINE.prompt().toLowerCase();
 
-  while (!(repeat === 'yes' || repeat === 'no')) {
+  while (!(answer === 'yes' || answer === 'no')) {
     console.log('Invalid input, please enter either \'yes\' or \'no\'...');
-    repeat = READLINE.prompt().toLowerCase();
+    answer = READLINE.prompt().toLowerCase();
   }
+  return answer;
+}
 
-} while (repeat === 'yes');
+console.log(greet.welcome);
+
+do {
+
+  let loan = getLoan();
+
+  let months = getLoanDuration();
+
+  let interest = getAPR();
+
+  console.log(findRate(loan, months, interest));
+
+  answer = repeat();
+
+} while (answer === 'yes');
 console.log(greet.exit);
